34371ea72495d32015849e4fbed671ce92580d95
nick <nicholas_scalione@yahoo.com>
2021-01-17 21:09:49 -0500

    commit tree a3bcc701ad65e231b3a6ee39980bd12e98baf0d9

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(number)
+ if number.integer? == false
+ return false  
+elsif number < 2
+    return false  
+elsif number == 2
+    return true 
+else
+    (2..number-1).each {|int| return false if number % int == 0}
+    return true  
+end
+end
